754afd104c0c02a6f40e5844b99532cf97e9a8a1
Chris Boettner <boettnec@gmail.com>
2023-01-26 09:56:10 +0100

    started with deep q learning

--- script.py
+++ script.py
@@ -10,7 +10,5 @@ from maze import MazeGame
 from qagent import QAgent
 
 if __name__ == "__main__":
-    to go to deep q learning, open new git branch
-
     maze = MazeGame()
     qagent = QAgent(maze)
